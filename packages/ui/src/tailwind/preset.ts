a6aad531a02496b36313b3774990d7e39e4a6010
Jadzixpl <jadziaz445@gmail.com>
2026-05-24 16:02:08 +0200

    PZPP-52 Added three color palette
    Added three color themes. Made the themes dynamic.

--- packages/ui/src/tailwind/preset.ts
+++ packages/ui/src/tailwind/preset.ts
@@ -1,7 +1,21 @@
 import type { Config } from "tailwindcss";
 
 const preset: Partial<Config> = {
-  // Tailwind preset config goes here
+  theme: {
+    extend: {
+      colors: {
+        text: "var(--color-text)",
+        bg: "var(--color-bg)",
+        sidebar: "var(--color-sidebar)",
+        primary: "var(--color-primary)",
+        error: "var(--color-error)",
+        warning: "var(--color-warning)",
+        success: "var(--color-success)",
+        info: "var(--color-info)",
+      },
+    },
+  },
+  plugins: [],
 };
 
 export default preset;
