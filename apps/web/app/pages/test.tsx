0d9c80f5aa488bcf9536b85915724fc4de30e7bd
Lucifer <SurdackiD1@gmail.com>
2026-05-24 11:57:06 +0200

    Revert "PZPP2-50  Added shadcn generator"
    This reverts commit d1de7af3884259ebaec7927c9e6575d4b4940691.

--- apps/web/app/pages/test.tsx
+++ apps/web/app/pages/test.tsx
@@ -2,14 +2,7 @@ import { useEffect, useState } from "react";
 import type { IDiagnosticsService } from "@basalt/core/interfaces/IDiagnosticsService";
 import { diagnosticsService } from "@basalt/core/services/DiagnosticsService";
 import { TestComponent } from "@basalt/ui";
-import {
-  Card,
-  CardHeader,
-  CardTitle,
-  CardDescription,
-  CardContent,
-  CardFooter,
-} from "~/components/ui/card";
+
 type HealthData = Awaited<ReturnType<IDiagnosticsService["healthcheck"]>>;
 
 export function Test() {
@@ -22,22 +15,9 @@ export function Test() {
   }, []);
 
   return (
-    <div className="space-y-6">
+    <div>
       <p>Page used for testing the routing <a href="/">Back to home</a></p>
-
-      <Card>
-        <CardHeader>
-          <CardTitle>Test Card</CardTitle>
-          <CardDescription>Sample component from @basalt/ui</CardDescription>
-        </CardHeader>
-        <CardContent>
-          Test card
-        </CardContent>
-        <CardFooter>Footer</CardFooter>
-      </Card>
-
       <TestComponent title="Test Card" description="Sample component from @basalt/ui" />
-
       {health && <pre>{JSON.stringify(health, null, 2)}</pre>}
     </div>
   );
