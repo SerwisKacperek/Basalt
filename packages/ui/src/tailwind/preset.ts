78a76a48ce3ffc3e1f8abc2912bdee3dab4bcf90
Jadzixpl <jadziaz445@gmail.com>
2026-05-27 22:59:10 +0200

    PZPP-52 Minor style changes, compatible with shadcn components
    Added border-radius, fonts, spacings, buttons font color. Everything automatically aplies to shadcn components.

--- packages/ui/src/tailwind/preset.ts
+++ packages/ui/src/tailwind/preset.ts
@@ -13,6 +13,16 @@ const preset: Partial<Config> = {
         success: "var(--color-success)",
         info: "var(--color-info)",
       },
+      fontFamily: {
+        sans: ["var(--font-sans)", "sans-serif"],
+        heading: ["var(--font-heading)", "sans-serif"],
+      },
+      spacing: {
+        xs: "0.25rem",
+        sm: "0.5rem",
+        md: "1rem",
+        lg: "2rem",
+      },
     },
   },
   plugins: [],
