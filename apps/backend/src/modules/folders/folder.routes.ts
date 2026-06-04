37ffd61d357cb639a9ed3a97b273c41b3496ec53
Kacper Baum <Kaba8081@gmail.com>
2026-06-04 20:22:39 +0200

    PZPP2-77 - Folder Routes + rename

---
+++ apps/backend/src/modules/folders/folder.routes.ts
@@ -0,0 +1,43 @@
+import { Elysia, t } from 'elysia';
+
+import type { Db } from '../../shared/factories/db.factory';
+import type { IController } from '../../shared/interfaces/controller.base';
+import { FolderController } from './folder.controller';
+import { FolderService } from './folder.service';
+import { FolderRepository } from './folder.repository';
+import { errorHandler } from '@/shared/middleware';
+import { FolderBody, FolderResponse } from '@/schema/tables/folders/folders.schema';
+
+export const createFolderRoutes = (
+  db: Db,
+  controller: IController<'folders'> = new FolderController(new FolderService(new FolderRepository(db)))
+) => {
+  return new Elysia({ prefix: '/folders ' })
+    .use(errorHandler)
+    .get('/', () => controller.getAll(), {
+      response: t.Array(FolderResponse),
+    })
+    .get('/:id', ({ params }) => controller.getById(params.id), {
+      params: t.Object({ id: t.String() }),
+      response: FolderResponse,
+    })
+    .post('/', ({ body, set }) => {
+      set.status = 201;
+      return controller.create(body as any);
+    }, {
+      body: FolderBody,
+      response: { 201: FolderResponse },
+    })
+    .patch('/:id', ({ params, body }) => controller.update(
+      params.id,
+      body as any,
+    ), {
+      params: t.Object({ id: t.String() }),
+      body: FolderBody,
+      response: FolderResponse,
+    })
+    .delete('/:id', ({ params }) => controller.remove(params.id), {
+      params: t.Object({ id: t.String() }),
+      response: t.Void(),
+    })
+}
---
+++ apps/backend/src/schema/tables/folders/folders.schema.ts
@@ -0,0 +1,24 @@
+import { t } from 'elysia';
+import {
+  createInsertSchema,
+  createSelectSchema,
+  createUpdateSchema
+} from 'drizzle-typebox';
+
+import { schema } from '../../../shared/factories/schema.factory';
+
+const createFolderSelectSchema: any = createSelectSchema;
+const createFolderInsertSchema: any = createInsertSchema;
+const createFolderUpdateSchema: any = createUpdateSchema;
+
+const folders = schema.folders;
+
+export const FolderResponse = createFolderSelectSchema(folders);
+export const FolderBody = t.Pick(
+  createFolderInsertSchema(folders),
+  ['folder_id', 'workspace_id', 'name'],
+);
+export const FolderUpdateBody = t.Pick(
+  createFolderUpdateSchema(folders),
+  ['folder_id', 'workspace_id', 'name'],
+)
