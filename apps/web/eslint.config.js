eaa44f8d5520aac310d42108341ca4bf62aab736
ctx3 <ctx4@proton.me>
2026-06-05 18:38:54 +0200

    PZPP2-81 (#22)

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,3 +1,4 @@
+import { globalIgnores } from "eslint/config";
 import { config } from "@repo/eslint-config/react-app";
 
-export default [...config];
+export default [...config, globalIgnores([".dist/**"])];
